3a84362271c15439a5caa71d5b4d81ef582d5889
Maurillugdar <maurillugdar@gmail.com>
2024-05-09 18:22:14 -0300

    PreetyPrint

--- src/feed/Article.java
+++ src/feed/Article.java
@@ -1,5 +1,13 @@
 package feed;
 
 public class Article {
-    // TODO
+    private String title, description, pubDate, link;
+
+    public String print(){
+        return "Title: " + title + "\n" +
+                "Description: " + description + "\n" +
+                "Publication Date: " + pubDate + "\n" +
+                "Link: " + "\n" + link + "\n" + "*".repeat(80);
+    }
 }
+
