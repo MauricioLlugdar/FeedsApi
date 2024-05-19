3be6bea739345847e3ce57054de941033e1d55e9
Maurillugdar <maurillugdar@gmail.com>
2024-05-19 01:24:50 -0300

    parseXML now Working, and printing Feeds working too, simplemente el GOAT

--- src/feed/FeedParser.java
+++ src/feed/FeedParser.java
@@ -2,14 +2,12 @@ package feed;
 
 import org.w3c.dom.Document;
 import org.w3c.dom.NodeList;
-import org.w3c.dom.Node;
 import org.w3c.dom.Element;
+import org.w3c.dom.Node;
 
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.*;
 import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
@@ -21,21 +19,22 @@ public class FeedParser {
     public static List<Article> parseXML(String xmlData) {
         List<Article> articles = null;
         try {
+
             DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
             DocumentBuilder dBuilder = dbFactory.newDocumentBuilder();
-            Document doc = dBuilder.parse(xmlData);
-            doc.getDocumentElement().normalize();
-            System.out.println("Root element :" + doc.getDocumentElement().getNodeName());
+            InputStream is = new ByteArrayInputStream(xmlData.getBytes("UTF-8")); //Clave
+            Document doc = dBuilder.parse(is);
             NodeList nList = doc.getElementsByTagName("item");
             articles = new ArrayList<Article>();
 
             for (int nArt = 0; nArt < nList.getLength(); nArt++) {
                 Article actArt = new Article();
-                Element item = (Element) nList.item(nArt);
-                actArt.setLink(item.getAttribute("link"));
-                actArt.setTitle(item.getAttribute("title"));
-                actArt.setDescription(item.getAttribute("description"));
-                actArt.setPubDate(item.getAttribute("pubDate"));
+                Element element = (Element) nList.item(nArt);
+                //System.out.println("Este es el link = " + element.getElementsByTagName("description").item(0).getTextContent());
+                actArt.setLink((element.getElementsByTagName("link").item(0) !=null) ? element.getElementsByTagName("link").item(0).getTextContent() : "");
+                actArt.setTitle((element.getElementsByTagName("title").item(0) !=null) ? element.getElementsByTagName("title").item(0).getTextContent() : "");
+                actArt.setDescription((element.getElementsByTagName("description").item(0) !=null) ? element.getElementsByTagName("description").item(0).getTextContent() : "");
+                actArt.setPubDate((element.getElementsByTagName("pubDate").item(0) !=null) ? element.getElementsByTagName("pubDate").item(0).getTextContent() : "");
                 articles.add(actArt);
             }
         } catch (Exception e) {
@@ -53,7 +52,7 @@ public class FeedParser {
         connection.setRequestProperty("Content-Type", "application/json");
 
         // Si todos los grupos usan el mismo user-agent, el servidor puede bloquear las solicitudes.
-        connection.setRequestProperty("grupo1", "lab_paradigmas");
+        connection.setRequestProperty("User-agent", "lab_paradigmas");
         connection.setConnectTimeout(5000);
         connection.setReadTimeout(5000);
 
