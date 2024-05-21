488e3fe31102ea1f70411fc165c763924122e908
nicocag29 <nicolas.cagliero@mi.unc.edu.ar>
2024-05-21 10:39:43 -0300

    comentarios sobre el funcionamiento de parseXML

--- src/feed/FeedParser.java
+++ src/feed/FeedParser.java
@@ -22,19 +22,26 @@ public class FeedParser {
 
             DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
             DocumentBuilder dBuilder = dbFactory.newDocumentBuilder();
-            InputStream is = new ByteArrayInputStream(xmlData.getBytes("UTF-8")); //Clave
-            Document doc = dBuilder.parse(is);
-            NodeList nList = doc.getElementsByTagName("item");
+            InputStream is = new ByteArrayInputStream(xmlData.getBytes("UTF-8")); // Obtenemos los bytes de xmlData para luego parsearlo
+            Document doc = dBuilder.parse(is); // Parseo del InputStream creado en un Document
+            NodeList nList = doc.getElementsByTagName("item"); // Obtenemos "nodos" a partir del documento, se crea un nodo por cada instancia de "item" que aparezca
             articles = new ArrayList<Article>();
 
+            // Cada Nodo representa un artículo, se creará una instancia de Article en cada iteracion del for y se cargarán los datos correspondientes
             for (int nArt = 0; nArt < nList.getLength(); nArt++) {
                 Article actArt = new Article();
                 Element element = (Element) nList.item(nArt);
                 //System.out.println("Este es el link = " + element.getElementsByTagName("description").item(0).getTextContent());
+
+                //Seteamos el link: a cada nodo le buscamos instancias de "link" y nos fijamos la primer instancia. En caso de no existir, queda un espacio en blanco
                 actArt.setLink((element.getElementsByTagName("link").item(0) !=null) ? element.getElementsByTagName("link").item(0).getTextContent() : "");
+                //Seteamos el title: a cada nodo le buscamos instancias de "title" y nos fijamos la primer instancia. En caso de no existir, queda un espacio en blanco
                 actArt.setTitle((element.getElementsByTagName("title").item(0) !=null) ? element.getElementsByTagName("title").item(0).getTextContent() : "");
+                //Seteamos el Descripcion: a cada nodo le buscamos instancias de "Descripcion" y nos fijamos la primer instancia. En caso de no existir, queda un espacio en blanco
                 actArt.setDescription((element.getElementsByTagName("description").item(0) !=null) ? element.getElementsByTagName("description").item(0).getTextContent() : "");
+                //Seteamos el PubDate: a cada nodo le buscamos instancias de "PubDate" y nos fijamos la primer instancia. En caso de no existir, queda un espacio en blanco
                 actArt.setPubDate((element.getElementsByTagName("pubDate").item(0) !=null) ? element.getElementsByTagName("pubDate").item(0).getTextContent() : "");
+                //Agregagos el artículo a la lista
                 articles.add(actArt);
             }
         } catch (Exception e) {
